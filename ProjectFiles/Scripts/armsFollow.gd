32e603926226f05eb27eac16385488251a8a3695
Nicccccc <nickshacheng.0921@gmail.com>
2019-07-30 22:28:44 -0700

    made arm that follows mouse and shoots

--- ProjectFiles/Scripts/armsFollow.gd
+++ ProjectFiles/Scripts/armsFollow.gd
@@ -4,7 +4,7 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var Mouse_Position
-
+var Real_Mouse_Position
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -12,5 +12,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	Mouse_Position = get_local_mouse_position()
+	Real_Mouse_Position = global_position.angle_to_point(get_global_mouse_position())
 	rotation += Mouse_Position.angle()
+	print(Real_Mouse_Position)
+	if(Real_Mouse_Position < PI/2 && Real_Mouse_Position > -PI/2):
+		$Sprite.flip_v = true
+	else:
+		$Sprite.flip_v = false
 
